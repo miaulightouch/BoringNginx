1adf8564e2da7c0f357d5ba3798f8432008f4c53
vagrant <vagrant@localhost.localdomain>
2017-03-02 00:59:18 +0000

    update to latest version

--- README.md
+++ README.md
@@ -2,6 +2,11 @@ PatchedNginx
 =========
 * forked from [BoringNginx](https://github.com/ajhaydock/BoringNginx)
 
+Supported Version:
+* CentOS 7:
+  * Nginx 1.10.3
+  * Nginx 1.11.10
+
 <img align="right" src="https://raw.githubusercontent.com/miaulightouch/PatchedNginx/master/nginx.png" alt="Nginx Logo" title="Nginx">
 
 Build script to build current stable Nginx with SSL source list below:
@@ -10,17 +15,19 @@ Build script to build current stable Nginx with SSL source list below:
 * OpenBSD's LibreSSL
 
 And provide addition feature:
-* add spdy support (from [@felixbuenemann](https://github.com/felixbuenemann/sslconfig/blob/7c23d2791857f0b07e3008ba745bcf48d8d6b170/patches/nginx_1_9_15_http2_spdy.patch))
+* add spdy support (from [@cujanovic](https://github.com/cujanovic/nginx-http2-spdy-patch))
 * build with passenger module [**PLEASE READ BELOW**](#enabling-passenger-for-ruby-on-rails)
 * build with full relro
 
 This allows you to use some state-of-the-art crypto features not yet available in the stable branch of OpenSSL, like [ChaCha20-Poly1305](https://boringssl.googlesource.com/boringssl/+/de0b2026841c34193cacf5c97646b38439e13200) as a cipher/MAC combo, and [X25519](https://boringssl.googlesource.com/boringssl/+/4fb0dc4b031df7c9ac9d91fc34536e4e08b35d6a) (aka Curve25519) as the ECDHE curve provider if you want to get away from using [unsafe NIST curves](https://safecurves.cr.yp.to/) (though you probably want to check the X25519 [browser support matrix](https://www.chromestatus.com/feature/5682529109540864) before trying that).
 
-It would compat as DEB/RPM package, you can easily install/uninstall via package manager.
+It would compat as RPM package, you can easily install/uninstall via package manager.
 
 ### Build specified version
 ```
 Usage: build-*.sh [Option]...
+
+--mainline     Build Nginx Mainline
 
 --boringssl    Use BoringSSL source
 --libressl     Use LibreSSL source
@@ -31,21 +38,11 @@ Usage: build-*.sh [Option]...
 --hardening    Enable full relro
 ```
 
-If you execute `./build-*.sh` without parameter, it would be `./build-*.sh 1.10.1 --openssl`
+If you execute `./build-*.sh` without parameter, it would be `./build-*.sh --openssl`
 
-For example, if you want build 1.11.2 with LibreSSL, passenger module, enable full relro:
+For example, if you want build stable version with LibreSSL, passenger module, enable full relro:
 
-`./build-centos.sh 1.11.2 libressl passenger hardening`
-
-You can specify newer nginx version, but it may not work.
-
-| Version      | Tested On     |          |
-|--------------|---------------|----------|
-| Nginx 1.10.0 | Debian Jessie | CentOS 7 |
-| Nginx 1.10.1 | Debian Jessie | CentOS 7 |
-| Nginx 1.11.0 | Debian Jessie | CentOS 7 |
-| Nginx 1.11.1 | Debian Jessie | CentOS 7 |
-| Nginx 1.11.2 | Debian Jessie | CentOS 7 |
+`./build-centos.sh libressl passenger hardening`
 
 ### Enabling Passenger (for Ruby-on-Rails)
 To enable [Phusion Passenger](https://www.phusionpassenger.com/) in Nginx, you need to compile the Passenger module into Nginx. Passenger has a helpful script to do this for you (`passenger-install-nginx-module`), but that makes it difficult to . Instead, I have developed a version of this script tweaked for Passenger that you can run after installing the Passenger gem and hopefully enable full Passenger support in Nginx.
--- build-centos.sh
+++ build-centos.sh
@@ -4,79 +4,75 @@ shopt -s extglob
 if [ "$(id -u)" -eq 0 ]; then echo -e "This script is not intended to be run as root.\nExiting." && exit 1; fi
 
 
-# ngxversion "nginx version"
-function ngxversion() {
-    echo ""
-    echo "Nginx version: $1"
-    string="@($supver)"
-    case $1 in
-        $string ) echo "It's Supported version.";;
-        *) echo "Unknown version, try default settings.";;
-    esac
-    echo ""
-    case "${ngxver#1.*}" in
-        *.0) spec=spec.0.patch;;
-        *.1|11.2) spec=spec.1.patch;;
-        11.3) spec=spec.3.patch;;
-    esac
-}
-
-
 # Default settings
 rpath="$(cd $(dirname $0) && pwd)" # Run path
 bdir="/tmp/patchednginx-$RANDOM" # Set build directory
-ngxver="1.10.1" # Default nginx version
 
-supver="1.10.0|1.11.0|1.10.1|1.11.1|1.11.2" # Supported version
+ngxver=1.10.3                                                    # Nginx stable version
+spdy=nginx-spdy.patch
+dtls=nginx__dynamic_tls_records.patch
 
-opensslver=1.0.2h                                                # OpenSSL version
-libresslver=2.4.2                                                # LibreSSL version
-chacha20=openssl__chacha20_poly1305_draft_and_rfc_ossl102g.patch # CF ChaCha20_Poly1305 patch
-spdy=nginx_1_9_15_http2_spdy.patch                               # updated CF SPDY patch
-spec=spec.1.patch                                                # RPM spec patch
+boring=boring.patch                                              # boringssl patch
+opensslver=1.0.2k                                                # OpenSSL version
+libresslver=2.4.5                                                # LibreSSL version
+chacha20=openssl__chacha20_poly1305_draft_and_rfc_ossl102j.patch # CF ChaCha20_Poly1305 patch
 
+spec=spec.patch                                                  # RPM spec patch
+
+
+
+mainlinever=1.11.10                                              # Nginx Mainline version
+mainline_spdy=nginx-spdy-1.11.10.patch
+mainline_dtls=nginx__1.11.5_dynamic_tls_records.patch
+mainline_boring=boring.patch
 
 # Handle arguments passed to the script. Currently only accepts the flag to
 # include passenger at compile time,but I might add a help section or more options soon.
 while [ "$#" -gt 0 ]
 do
     case $1 in
-        --hardening|-hardening|hardening) HARDENING="1"; shift;;
         --passenger|-passenger|passenger) PASSENGER="1"; echo ""; echo "Phusion Passenger module enabled."; shift;;
         --boringssl|-boringssl|boringssl) BORINGSSL="1"; shift;;
         --openssl|-openssl|openssl) OPENSSL="1"; shift;;
         --libressl|-libressl|libressl) LIBRESSL="1"; shift;;
-        1.*) ngxver=$1; shift;;
+        --mainline|-mainline|mainline)
+            Mainline=mainline/;
+            ngxver=$mainlinever;
+            spdy=$mainline_spdy;
+            dtls=$mainline_dtls;
+            boring=$mainline_boring;
+            shift;;
         *)
             echo "Invalid argument: $1"
             echo ""
             echo "Usage: $0 [Option]..."
+            echo ""
+            echo "--mainline     Build Nginx Mainline"
             echo ""
             echo "--boringssl    Use BoringSSL source"
             echo "--libressl     Use LibreSSL source"
             echo "--openssl      Use OpenSSL source with ChaCha20_Poly1305 patch"
             echo ""
             echo "--passenger    Build with passenger module"
-            echo ""
-            echo "--hardening    Enable full relro"
             exit 10
         ;;
     esac
 done
-ngxversion $ngxver
 [ ! "$BORINGSSL" -o "$LIBRESSL" ] && OPENSSL="1" # Use OpenSSL if not set
-
 
 # Prompt our user before we start removing stuff
 CONFIRMED=0
+echo ""
 echo -e "This script will remove any versions of Nginx you installed using yum, and\nreplace any version of Nginx built with a previous version of this script."
+echo -e "\nNginx Version: $ngxver"
 while true
 do
     echo ""
     read -p "Do you wish to continue? (Y/N) " answer
     case $answer in
         [yY]* ) CONFIRMED=1; break;;
-        * ) echo "Please enter 'Y' to continue or use ^C to exit.";;
+        [nN]* ) exit 10; break;;
+        * ) echo "Please enter 'Y' to continue or ^C to exit.";;
     esac
 done
 if [ "$CONFIRMED" -eq 0 ]; then echo -e "Something went wrong.\nExiting." && exit 1; fi
@@ -90,12 +86,11 @@ sudo yum -y install patch gcc gcc-c++ rpm-build perl-devel perl-ExtUtils-Embed G
 
 # Prepare source
 mkdir "$bdir" && mkdir -p "$rpath/source" && cd "$rpath/source"
-[ $(($(expr $ngxver : '..\([0-9]*\).*')%2)) -eq 1 ] && Mainline=mainline/
-[ ! -e nginx-$ngxver-1.el7.ngx.src.rpm ] && curl -LO --retry 3 "http://nginx.org/packages/${Mainline}centos/7/SRPMS/nginx-${ngxver}-1.el7.ngx.src.rpm"
+curl -LO --retry 3 "http://nginx.org/packages/${Mainline}centos/7/SRPMS/nginx-${ngxver}-1.el7.ngx.src.rpm"
 rpm -ih --define "_topdir $bdir" "nginx-$ngxver-1.el7.ngx.src.rpm"
 
 cp "${rpath}/patches/${spdy}" "${bdir}/SOURCES"
-
+cat "${rpath}/patches/${dtls}" >> "${bdir}/SOURCES/${spdy}"
 
 # Prepare SSL
 if [ $OPENSSL ]
@@ -128,11 +123,11 @@ then
 
     cp -r "${rpath}/boringssl" "$bdir/SOURCES"
     mkdir -p "${bdir}/SOURCES/boringssl/build" && cd "${bdir}/SOURCES/boringssl/build"
-    cmake ../ && make
+    cmake -DCMAKE_C_FLAGS=-fPIC -DCMAKE_CXX_FLAGS=-fPIC ../ && make
     mkdir -p "${bdir}/SOURCES/boringssl/.openssl/lib"
     cd "${bdir}/SOURCES/boringssl/.openssl" && ln -s ../include
     cd "${bdir}/SOURCES/boringssl" && cp "build/crypto/libcrypto.a" "build/ssl/libssl.a" ".openssl/lib"
-    cat "${rpath}/patches/boring.patch" >> "${bdir}/SOURCES/$spdy"
+    cat "${rpath}/patches/${boring}" >> "${bdir}/SOURCES/$spdy"
 
     SSLPACK="boringssl"
 fi
@@ -142,10 +137,11 @@ then
     mkdir -p "${rpath}/libressl" && cd "${rpath}/libressl"
     until [ "$VALID" == "1" ]; do
         [ $WRONG ] && curl -LO --retry 3 "http://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-${libresslver}.tar.gz"
-        curl -LO --retry 3 "http://ftp.openbsd.org/pub/OpenBSD/LibreSSL/SHA256" 
+        curl -LO --retry 3 "http://ftp.openbsd.org/pub/OpenBSD/LibreSSL/SHA256"
         grep libressl-${libresslver}.tar.gz SHA256 | sha256sum -c && VALID=1 || WRONG=1
     done
     tar xf "${rpath}/libressl/libressl-${libresslver}.tar.gz" -C "${bdir}/SOURCES"
+    sed -i 's/\.\/configure/CFLAGS=-fPIC \.\/configure/g' "${bdir}/SOURCES/libressl-${libresslver}/config"
 
     SSLPACK="libressl-${libresslver}"
 fi
@@ -162,7 +158,6 @@ fi
 # Setup RPM Spec
 patch "${bdir}/SPECS/nginx.spec" "${rpath}/patches/$spec"
 [ $PASSENGER ] && EXTRACONFIG="$EXTRACONFIG --add-module=$(passenger-config --root)/src/nginx_module"
-[ $HARDENING ] && EXTRACONFIG="$EXTRACONFIG --with-ld-opt='-Wl,-z,now'"
 sed -i "1 i\%define EXTRACONFIG ${EXTRACONFIG-;}" "${bdir}/SPECS/nginx.spec"
 sed -i "1 i\%define SSLPACK ${SSLPACK}" "${bdir}/SPECS/nginx.spec"
 sed -i "1 i\%define SPDY_PATCH $spdy" "${bdir}/SPECS/nginx.spec"
